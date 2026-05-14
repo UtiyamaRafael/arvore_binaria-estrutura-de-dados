3a53dc6aea838ce33b27a4c083efa6aa51a05eba
UtiyamaRafael <rafaelhu763@gmail.com>
2026-05-13 21:18:53 -0300

    Refactor removal methods and improve logic

--- ArvoreBinaria.java
+++ ArvoreBinaria.java
@@ -9,31 +9,30 @@ public class ArvoreBinaria {
     public void inserir(Integer conteudo) {
         No novoNo = new No(conteudo);
 
-        if(estaVazia()) {
+        if (estaVazia()) {
             this.raiz = novoNo;
         } else {
             No aux = this.raiz;
-            while(true) {
-                if(conteudo < aux.getConteudo()) {
-                    if(aux.getNoL() != null) {
+            while (true) {
+                if (conteudo < aux.getConteudo()) {
+                    if (aux.getNoL() != null) {
                         aux = aux.getNoL();
                     } else {
                         aux.setNoL(novoNo);
                         return;
                     }
                 } else if (conteudo > aux.getConteudo()) {
-                    if(aux.getNoR() != null) {
+                    if (aux.getNoR() != null) {
                         aux = aux.getNoR();
                     } else {
                         aux.setNoR(novoNo);
                         return;
                     }
-                }else {
+                } else {
                     System.out.println("Nó repetido - " + conteudo);
                     return;
                 }
             }
-
         }
     }
 
@@ -46,64 +45,119 @@ public class ArvoreBinaria {
     }
 
     public void posOrdem(No no) {
-        if(no == null) {
+        
+        if (no == null) {
             return;
         }
+
         posOrdem(no.getNoL());
         posOrdem(no.getNoR());
+
         System.out.println(no.getConteudo());
     }
 
     public void preOrdem(No no) {
-        if(no == null) {
+        if (no == null) {
             return;
         }
     }
 
-    public void remover(Integer conteudo){
-
+    public void remover(Integer conteudo) {
         if (estaVazia()) {
             return;
-        }else {
+        } else {
             String tipoRemocao = remocaoTipo(conteudo);
+
             switch (tipoRemocao) {
                 case "folha":
                     removerFolha(conteudo);
                     break;
-                case "FilhoR":
-                    removerFilhoR(conteudo);
+                case "umFilhoL":
+                    remover1filhoL(conteudo);
                     break;
-                case "FilhoL":
-                    removerFIlhoL(conteudo);
+                case "umFilhoR":
+                    remover1filhoR(conteudo);
                     break;
                 case "doisFilhos":
                     remover2filho(conteudo);
                     break;
+                case "noNaoEncontrado":
+                    System.out.println("Nó nao encontrado");
+                    break;
             }
         }
+    }
+
+    public String remocaoTipo(Integer conteudo) {
+
+        while (true) {
+            No aux = this.raiz;
+            
+            if (conteudo < aux.getConteudo()) {
+                if (aux.getConteudo().equals(conteudo)) {
+                    if (aux.getNoL() != null && aux.getNoR() != null) {
+                        return "doisFilhos";
+                    } else if (aux.getNoL() != null && aux.getNoR() == null) {
+                        return "umFilhoL";
+                    } else if (aux.getNoR() != null && aux.getNoL() == null) {
+                        return "umFilhoR";
+                    } else {
+                        return "folha";
+                    }
+                } else {
+                    aux = aux.getNoL();
+                }
+            } else if (conteudo > aux.getConteudo()) {
+                if (aux.getConteudo().equals(conteudo)) {
+                    if (aux.getNoL() != null && aux.getNoR() != null) {
+                        return "doisFilhos";
+                    } else if (aux.getNoL() != null && aux.getNoR() == null) {
+                        return "umFilhoL";
+                    } else if (aux.getNoR() != null && aux.getNoL() == null) {
+                        return "umFilhoR";
+                    } else {
+                        return "folha";
+                    }
+                } else {
+                    aux = aux.getNoR();
+                }
+            } else {
+                return "nóNaoEncontrado";
+            }
+        }
+    }
+
+    public void removerFolha(Integer conteudo) {
+        No aux = this.raiz;
+
+        while (true) {
+            if (conteudo < aux.getConteudo()) {
+                if (aux.getNoL().equals(conteudo)) {
+                    aux.setNoL(null);
+                    return;
+                } else {
+                    aux = aux.getNoL();
+                }
+            } else if (conteudo > aux.getConteudo()) {
+                if (aux.getNoR().equals(conteudo)) {
+                    aux.setNoR(null);
+                    return;
+                } else {
+                    aux = aux.getNoR();
+                }
+            }
+        }
+    }
+
+    public void remover1filhoL(Integer conteudo) {
 
     }
 
-    public String remocaoTipo(Integer conteudo){
-        return "a";
-    }
-
-    public void removerFilhoR(Integer conteudo){
+    public void remover1filhoR(Integer conteudo) {
 
     }
 
-    public void removerFolha(Integer conteudo){
+    public void remover2filho(Integer conteudo) {
 
     }
-
-    public void removerFilhoL(Integer conteudo){
-
-    }
-
-    public void remover2filho(Integer conteudo){
-
-    }
-
-
-
 }
